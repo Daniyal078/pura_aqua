43cb6efa59824e3754c56d7e2bbc1c12ddde8e07
Daniyal078 <102375571+Daniyal078@users.noreply.github.com>
2022-08-15 15:26:43 +0500

    first poka

---
+++ js/app-data.js
@@ -0,0 +1,127 @@
+const CATEGORIES = [
+    {
+        id: 1,
+        name: 'Bottles'
+    },
+    {
+        id: 2,
+        name: 'Thermos'
+    }
+];
+const PRODUCTS = [
+    {
+        id: 1,
+        img: 'images/img099.webp',
+        title: '32 oz Wide Mouth',
+        description: '32 oz Wide Mouth w/ Straw Lid is rated 4.7 out of 5 by 3133.',
+        price: 1250,
+    },
+    {
+        id: 2,
+        img: 'images/img08.JPEG',
+        title: '32 oz Wide Mouth',
+        description: '32 oz Wide Mouth is rated 4.6 out of 5 by 3404.',
+        price: 1250,
+    },
+    {
+        id: 3,
+        img: 'images/img05.JPG',
+        title: 'Straw Lid  plastic black 600 ml',
+        description: 'Keep yourself hydrated on the go with this.',
+        price: 1250,
+    },
+    {
+        id: 4,
+        img: 'images/img03-remove.png',
+        title: 'card title',
+        description: 'lorem lipsum',
+        price: 1250,
+    },
+    {
+        id: 5,
+        img: 'images/img08.webp',
+        title: 'card title',
+        description: 'lorem lipsum',
+        price: 1250,
+    },
+    {
+        id: 6,
+        img: 'images/img099.webp',
+        title: 'card title',
+        description: 'lorem lipsum',
+        price: 1250,
+    },
+    {
+        id: 7,
+        img: 'images/img10.png',
+        title: 'card title',
+        description: 'lorem lipsum',
+        price: 1250,
+    },
+    {
+        id: 8,
+        img: 'images/re-edit2.png',
+        title: 'card title',
+        description: 'lorem lipsum',
+        price: 1250,
+    },
+    {
+        id: 9,
+        img: 'images/img02.jpg',
+        title: 'card title',
+        description: 'lorem lipsum',
+        price: 4449,
+    },
+    {
+        id: 10,
+        img: 'images/img07-removebg.png',
+        title: 'card title',
+        description: 'lorem lipsum',
+        price: 2599,
+    },
+    {
+        id: 11,
+        img: 'images/re-edit.png',
+        title: 'card title',
+        description: 'lorem lipsum',
+        price: 2199,
+    },
+    {
+        id: 12,
+        img: 'images/img04.webp',
+        title: 'card title',
+        description: 'lorem lipsum',
+        price: 2199,
+    },
+];
+
+const ThermosProducts = [
+    {
+        thermos_id: 13,
+        img: 'images-2/img-thermos1.jpeg',
+        title: 'card title',
+        description: 'WATER BOTTLE 6039 4956',
+        price: 1250,
+    },
+    {
+        thermos_id: 14,
+        img: '../card title/images-2/img-thermos2.jpeg',
+        title: 'card title',
+        description: 'WATER BOTTLE 6039 4956',
+        price: 1250,
+    },
+    {
+        thermos_id: 15,
+        img: '../card title/images-2/thermos-img3.jpeg',
+        title: 'card title',
+        description: 'WATER BOTTLE 6039 4956',
+        price: 1250,
+    },
+    {
+        thermos_id: 16,
+        img: '../card title/images-2/thermos-img4.jpeg',
+        title: 'card title',
+        description: 'WATER BOTTLE 6039 4956',
+        price: 1250,
+    },
+]
